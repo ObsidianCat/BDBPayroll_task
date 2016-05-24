8295588b031518bce18758f041a000a7eb1dcaf1
Lula Leus <lula.leus@gmail.com>
2016-05-24 14:08:50 +0300

    Feat: work on adding comments

--- scripts/commentsCtrl.js
+++ scripts/commentsCtrl.js
@@ -3,14 +3,35 @@
  */
 angular.module('payrollApp').controller('commentsCtrl', [
     "$scope",
+    "$window",
     "DataHandler",
-    function($scope, DataHandler){
-        $scope.test = "hello!";
+    function($scope, $window, DataHandler){
         $scope.comments;
+        function createCommentModel(listLength){
+            return {
+                id:listLength+1,
+                title:"",
+                text:"",
+                "tags": ["bug", "issue", "etc"]
+            };
+        }
+
         DataHandler.getAllComments().then(function(response) {
             $scope.comments = response;
+            $scope.newComment = createCommentModel($scope.comments.length);
         });
 
+        $scope.addNewComment = function(){
+          if($scope.newComment.title.trim() !="" && $scope.newComment.text.trim() !=""){
+              $scope.comments.push($scope.newComment);
+              $scope.newComment.title = "";
+              $scope.newComment.text = "";
+              $scope.newComment.id +=1;
+          }
+          else{
+              $window.alert("Please fill title and text fields!");
+          }
+        };
         $scope.removeComment = function(index){
             $scope.comments.splice(index,1);
         };
